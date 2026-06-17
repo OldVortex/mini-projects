21babeaea240bd50830c745fa4e4a99440080037
Rishabh <rishabhnambiar2004@gmail.com>
2026-06-17 20:15:10 +0530

    Initial multiple client connections

--- ChatApp/server.py
+++ ChatApp/server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 HOST = "127.0.0.1"
 PORT = 5555
@@ -6,23 +7,31 @@ PORT = 5555
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((HOST, PORT))
-server.listen(1)
+server.listen()
 
 print(f"Server listening on {HOST}:{PORT}")
 
-client_socket, client_address = server.accept()
+def client_handler(client_socket):
+    while True:
+        message = client_socket.recv(1024).decode()
+        
+        if not message:
+            print("Client Disconnected.")
+            break
+        
+        client_socket.send(message.encode())
+        print(f"Client: {message}")
 
-print(f"Connected to {client_address}")
+    client_socket.close()
 
 while True:
-    message = client_socket.recv(1024).decode()
+    client_socket, client_address = server.accept()
+    print(f"Connected to {client_address}")
     
-    if not message:
-        break
-    
-    client_socket.send(message.encode())
+    thread = threading.Thread(
+        target = client_handler,
+        args = (client_socket,)
+    )
+    thread.start()
 
-    print(f"Client: {message}")
-
-client_socket.close()
 server.close()

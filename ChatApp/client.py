cc6ced2d3d81ab61423f3fd8362bdd1a6f270d68
Rishabh <rishabhnambiar2004@gmail.com>
2026-06-18 19:47:45 +0530

    Improved code

--- ChatApp/client.py
+++ ChatApp/client.py
@@ -3,10 +3,11 @@ import socket
 HOST = "127.0.0.1"
 PORT = 5555
 
+username = print("Enter username: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
 client.connect((HOST, PORT))
+client.send(username.encode())
 
 while True:
     message = input("Enter message: ")
--- ChatApp/server.py
+++ ChatApp/server.py
@@ -11,27 +11,36 @@ server.listen()
 
 print(f"Server listening on {HOST}:{PORT}")
 
-def client_handler(client_socket):
+def client_handler(client_socket, client_address):
+    username = client_socket.recv(1024).decode()
+    print(f"{username} connected from [{client_address[0]}:{client_address[1]}]")
+    
     while True:
         message = client_socket.recv(1024).decode()
         
         if not message:
-            print("Client Disconnected.")
+            print(f"Client [{client_address[0]}:{client_address[1]}] disconnected")
             break
         
+        print(f"[{client_address[0]}:{client_address[1]}]: {message}")
+        
         client_socket.send(message.encode())
-        print(f"Client: {message}")
 
     client_socket.close()
 
-while True:
-    client_socket, client_address = server.accept()
-    print(f"Connected to {client_address}")
+try:
+    while True:
+        client_socket, client_address = server.accept()
+        print(f"Connected to {client_address}")
     
-    thread = threading.Thread(
-        target = client_handler,
-        args = (client_socket,)
-    )
-    thread.start()
+        thread = threading.Thread(
+            target = client_handler,
+            args = (client_socket, client_address)
+        )
+        
+        thread.daemon = True
+        thread.start()
 
-server.close()
+except KeyboardInterrupt:
+    print("/nShutting down server....")
+    server.close()

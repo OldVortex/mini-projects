2977c468ca766bbed29d8830c429c94060148191
Rishabh <rishabhnambiar2004@gmail.com>
2026-06-20 20:08:58 +0530

    Improved display

--- ChatApp/server.py
+++ ChatApp/server.py
@@ -1,8 +1,13 @@
 import socket
 import threading
+import time
 
 HOST = "127.0.0.1"
 PORT = 5555
+
+def timestamp():
+    return time.strftime("%H:%M:%S")
+
 
 clients = []
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,22 +15,22 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
-print(f"Server listening on {HOST}:{PORT}")
+print(f"/nServer listening on {HOST}:{PORT}")
 
 def client_handler(client_socket, client_address):
     username = client_socket.recv(1024).decode()
-    print(f"{username} connected from [{client_address[0]}:{client_address[1]}]")
+    print(f"/n[{timestamp()}] [CONNECTED] {username} ({client_address[0]}:{client_address[1]})")
     clients.append(client_socket)
     
     while True:
         message = client_socket.recv(1024).decode()
         
         if not message:
-            print(f"Client [{client_address[0]}:{client_address[1]}] disconnected")
+            print(f"/n[{timestamp()}] [DISCONNECTED] {username}")
             clients.remove(client_socket)
             break
         
-        print(f"[{client_address[0]}:{client_address[1]} ({username})]: {message}")
+        print(f"/n[{timestamp()}] [MESSAGE] {username}: {message}")
         
         client_socket.send(message.encode())
 
@@ -34,14 +39,12 @@ def client_handler(client_socket, client_address):
 try:
     while True:
         client_socket, client_address = server.accept()
-        print(f"Connected to {client_address}")
     
         thread = threading.Thread(
             target = client_handler,
             args = (client_socket, client_address)
         )
-        
-        thread.daemon = True
+
         thread.start()
 
 except KeyboardInterrupt:
